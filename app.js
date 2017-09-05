35eed61775e194768e5975225eccc1e558021526
yuxuemei <yuxm@gvgcn.com>
2017-09-05 18:41:57 +0800

    接口处理 路径改变

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-import foo from './src/foo.vue'
+import foo from './src/pages/home/foo.vue'
 foo.el = '#root'
 export default new Vue(foo);
---
+++ src/pages/home/detail.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="header row">
+        
+    </div>
+</template>
+<script>
+	var stream = weex.requireModule('stream');
+  var modal = weex.requireModule('modal');
+  var navigator = weex.requireModule('navigator');
+  export default {
+    data: {
+      logoUrl: 'https://alibaba.github.io/weex/img/weex_logo_blue@3x.png'
+    },
+    mounted:function(){
+    	navigator.getParams(callback) //async
+        var param=navigator.getParams(); //sync
+        console.log(param);
+    },
+    methods: {
+    }
+  }
+</script>	
